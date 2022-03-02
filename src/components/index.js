0a5603f3208255ec571ee934da122bab124bf4eb
akshay4570 <akshay4570@gmail.com>
2022-03-02 23:02:56 +0530

    Changes

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as About} from './About/About'; 
+export {default as MainContent} from './MainContent/MainContent'; 
+export {default as Projects} from './Projects/Projects'; 
+export {default as Sidebar} from './Sidebar/Sidebar'; 
+export {default as WorkExperience} from './WorkExperience/WorkExperience'; 
+export {default as Work} from './Work/Work'; 
