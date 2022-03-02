f9b86aeba1dd19366dd667743cfb6439b9ef1381
akshay4570 <akshay4570@gmail.com>
2022-03-02 22:59:03 +0530

    First Commit

---
+++ src/constants/images.jsx
@@ -0,0 +1,25 @@
+import cpp from '../assets/cpp.png';
+import css from '../assets/css.png';
+import git from '../assets/git.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import node from '../assets/node.png';
+import profile from '../assets/profile.jpeg';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
+import Resume from '../assets/Resume.png';
+import sass from '../assets/sass.png';
+
+export default {
+    cpp,
+    css,
+    git,
+    html,
+    javascript,
+    node,
+    profile,
+    python,
+    react,
+    Resume,
+    sass,
+};
